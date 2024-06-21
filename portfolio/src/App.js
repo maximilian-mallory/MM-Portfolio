64ef16d2a32a078a55b4837a6d9d7525aa51eaad
Maximilian Mallory <maximilian.mallory@outlook.com>
2024-06-21 16:21:56 -0500

    Added Nav bar, routing for home about and contact, and created split screen components for the about page. Implemented resizing capabilitied for the split screen for better use of portrait monitor display. Currently loading information about self from the GitHub API.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,129 @@
-import logo from './logo.svg';
 import './App.css';
+import {NavBar} from './components/NavBar'
+import { Route, Routes, Outlet } from 'react-router-dom'
+import bannerImage from './assets/banner.jpg';
+import {useState, useEffect} from 'react';
+import { SplitScreen } from './components/SplitScreen';
+import headshot from './assets/headshot.jpg';
+
+function Home() {
+  return <h1>Home</h1>
+}
+
+
+
+function About() {
+
+  const [aspectRatio, setAspectRatio] = useState('landscape'); // Assume landscape by default
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const newAspectRatio = width >= height ? 'landscape' : 'portrait';
+      setAspectRatio(newAspectRatio);
+    };
+
+    handleResize(); 
+
+    window.addEventListener('resize', handleResize);
+    
+    console.log(`screen resized to ${aspectRatio}`)
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const getSplitScreenWeights = () => {
+    if (aspectRatio === 'portrait') {
+      return { leftWeight: 1, rightWeight: 2 };
+    } else {
+
+      return { leftWeight: 1, rightWeight: 4 };
+    }
+  };
+
+  return (
+  <SplitScreen {...getSplitScreenWeights()}>
+    <BioComponent />
+    <HistoryComponent/>
+    </SplitScreen>
+  )
+  
+}
+
+const HistoryComponent = () => {
+  return ( 
+    <div id='historyComponent'> 
+    <h1>History</h1>
+    </div>
+  )
+  
+  
+}
+
+const BioComponent = () => {
+  const [ data, setData ] = useState();
+  const [ error, setError ] = useState();
+  const [ loading, setLoading ] = useState();
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      `https://api.github.com/users/maximilian-mallory`,
+    )
+    .then((response) => response.json())
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError)
+  }, [])
+
+  if(loading) return <h1>Loading...</h1>
+  if(error) 
+    return <pre>{JSON.stringify(error)}</pre>;
+  if(!data) return null
+  
+  return ( 
+    <div id="bioComponent">
+      <div >
+      <h1>About Me</h1>
+      <h2>
+        {data.name}
+      </h2>
+      <img src={data.avatar_url} height={150} alt='avatar'/> {/* Github API won't always respond with the avatar image so I placed it in assets */}
+      <p>
+        {data.location}
+      </p>
+      <p>{data.bio}</p>
+      <a href={data.url}>Link to GitHub Profile</a>
+      </div>
+    </div>
+  )
+}
+
+function Contact() {
+  return <h1>Contact</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className='navBanner'/>
+    <NavBar/>
+    <Routes>
+      <Route 
+        path="/" 
+        element={<Home />}>
+      </Route>
+      <Route 
+        path="/about" 
+        element={<About />}>
+      </Route>
+      <Route 
+        path="/contact" 
+        element={<Contact />}>
+      </Route>
+    </Routes>
+    </>
+    
   );
 }
 
---
+++ portfolio/src/components/layouts.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const MainLayout = ({children}) => {
+    return(
+        <div>
+            <header>
+
+            </header>
+
+            <main>
+                <div>
+                    Main Content
+                </div>
+            </main>
+
+            <footer>
+
+            </footer>
+        </div>
+    )
+}
